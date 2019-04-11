80f0fd8752c5d0cce98b2052a6c1fc672baa0f62
Roi Otero Morales <roioteromorales@gmail.com>
2019-04-11 14:02:26 +0800

    Remove unused class and changed path of the app to work under root

--- src/main/java/com/codingnomads/betty/controller/HelloController.java
+++ src/main/java/com/codingnomads/betty/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sayHello() {
         return "hello";
     }
