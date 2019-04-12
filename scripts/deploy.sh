4be5fdbc28735973f7333039c62f27c447efdb1f
Roi <roioteromorales@gmail.com>
2019-04-12 17:28:26 +0800

    Playing with ls in Travis (#13)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,9 @@ echo "Starting file validation and SCP to Tomcat Server..."
 
 PEM_FILE=secrets/betty-webserver-key.pem
 DEPLOY_FOLDER=/opt/tomcat/latest/webapps/
+WAR_FILE=build/libs/betty-0.0.1-SNAPSHOT.war
 
 if [ ! -f "$WAR_FILE" ]; then
-    echo "----------------------->"
-    ls ../
-    echo "----------------------->"
-    ls
-    echo "----------------------->"
     echo "$WAR_FILE does not exist"
     exit 127
 fi
@@ -20,7 +16,7 @@ if [ ! -f "$PEM_FILE" ]; then
     exit 127
 fi
 
-scp -i $PEM_FILE $WAR_FILE ubuntu@3.19.108.19:"$DEPLOY_FOLDER"ROOT.war
+scp -o StrictHostKeyChecking=no -i $PEM_FILE $WAR_FILE ubuntu@3.19.108.19:"$DEPLOY_FOLDER"ROOT.war
 echo "PEACE TO WORLD"
 
 
