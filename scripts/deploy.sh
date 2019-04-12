6b80fcc1c0c9885123da53fca0e0821e1593a409
Roi Otero Morales <roioteromorales@gmail.com>
2019-04-12 16:19:56 +0800

    Created deployment script and encryption travis logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-echo "this should be deploying instead of just writing this text"
+echo "Starting file validation and SCP to Tomcat Server..."
+
+WAR_FILE=build/libs/betty-0.0.1-SNAPSHOT.war
+PEM_FILE=secrets/betty-webserver-key.pem
+DEPLOY_FOLDER=/opt/tomcat/latest/webapps/
+
+if [ ! -f "$WAR_FILE" ]; then
+    echo "$WAR_FILE does not exist"
+    exit 127
+fi
+
+if [ ! -f "$PEM_FILE" ]; then
+    echo "$PEM_FILE does not exist"
+    exit 127
+fi
+
+scp -i $PEM_FILE $WAR_FILE ubuntu@3.19.108.19:"$DEPLOY_FOLDER"ROOT.war
+echo "PEACE TO WORLD"
+
+
