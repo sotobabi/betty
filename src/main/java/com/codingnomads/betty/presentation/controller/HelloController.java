0278586f09f4ffdd2d1d053b938c18ac9cc76477
Cristian Perez <cristian.perez2011@gmail.com>
2019-04-12 14:35:57 +0800

    Added the sentiment score pojo

--- src/main/java/com/codingnomads/betty/presentation/controller/HelloController.java
+++ src/main/java/com/codingnomads/betty/presentation/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.codingnomads.betty.controller;
+package com.codingnomads.betty.presentation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
