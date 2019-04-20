d656ff4ebefe065e0acc7c5650b6f67bf0b7c8bf
snaket2003 <32600398+snaket2003@users.noreply.github.com>
2019-04-20 15:58:17 +0800

    Added test folders and two initial tests. Also, added doc for gradle-wrapper (#15)

---
+++ src/test/com/codingnomads/betty/BettyApplicationTests.java
@@ -0,0 +1,27 @@
+package com.codingnomads.betty;
+
+import com.codingnomads.betty.presentation.controller.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BettyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+    @Test
+    public void whenRootMappingIsCalled_helloTemplateIsReturned() {
+        HelloController testController = new HelloController();
+
+        assertEquals("hello", testController.sayHello());
+    }
+
+}
