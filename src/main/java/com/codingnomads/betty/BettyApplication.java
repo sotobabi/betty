7f20716a624d37e39a433ce467e68d00cdac392b
alpdurmaz <alp_durmaz@hotmail.com>
2019-04-24 10:49:40 +0800

    Twitter exception deleted from main method

--- src/main/java/com/codingnomads/betty/BettyApplication.java
+++ src/main/java/com/codingnomads/betty/BettyApplication.java
@@ -3,12 +3,11 @@ package com.codingnomads.betty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import twitter4j.TwitterException;
 
 @SpringBootApplication
 public class BettyApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws TwitterException {
+	public static void main(String[] args) {
 		SpringApplication.run(BettyApplication.class, args);
 	}
 }
