1f5198e9388f96282630dc1ebb1bb7d365b9e055
alpdurmaz <alp_durmaz@hotmail.com>
2019-04-24 16:39:33 +0800

    Test code fixed

--- src/test/java/com/codingnomads/betty/database/TwitterSourceRepositoryTest.java
+++ src/test/java/com/codingnomads/betty/database/TwitterSourceRepositoryTest.java
@@ -1,15 +1,13 @@
 package com.codingnomads.betty.database;
 
 import com.codingnomads.betty.configurations.TwitterConfigurer;
-import com.codingnomads.betty.database.TwitterSourceRepository;
-import com.codingnomads.betty.logic.repositories.TwitterRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {TwitterSourceRepository.class, TwitterConfigurer.class})
