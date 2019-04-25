e193414c1609531db1b63a9c3d1ab9bcebd835b5
Cristian Perez <cristian.perez2011@gmail.com>
2019-04-25 18:48:02 +0800

    Cleaning up debugging parameters since server is back up

--- src/main/java/com/codingnomads/betty/BettyApplication.java
+++ src/main/java/com/codingnomads/betty/BettyApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class BettyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		System.out.println("--------------------------------->");
-		System.out.println(System.getenv("DATABASE_USERNAME"));
 		SpringApplication.run(BettyApplication.class, args);
 	}
 }
