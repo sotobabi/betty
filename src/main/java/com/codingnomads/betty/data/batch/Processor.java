219d0918d4630ca056ea768276a515ae11028c47
snaket2003 <32600398+snaket2003@users.noreply.github.com>
2019-04-29 19:49:51 +0800

    Finished Initial Spring Batch implementation into Betty (#33)
    * Finished Spring Batch implementation without a scheduler or hard-coded fields
    
    * Added some tests and some sonar exclusions for models and JPA repository
    
    * Added more tests and exclusions. Need advise on how to create List of Status

---
+++ src/main/java/com/codingnomads/betty/data/batch/Processor.java
@@ -0,0 +1,42 @@
+package com.codingnomads.betty.data.batch;
+
+import com.codingnomads.betty.data.batch.models.StatusItemReader;
+import com.codingnomads.betty.data.models.Tweet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import twitter4j.Status;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class Processor implements ItemProcessor<List<Status>, List<Tweet>> {
+
+    private StatusItemReader statusItemReader;
+
+    @Autowired
+    public Processor(StatusItemReader statusItemReader) {
+        this.statusItemReader = statusItemReader;
+    }
+
+    @Override
+    public List<Tweet> process(List<Status> statusList) throws Exception {
+        List<Tweet> listOfTweets = new ArrayList<>();
+
+        for (Status status : statusList) {
+            if (status == null) {
+                return Collections.emptyList();
+            }
+            Tweet tweet = new Tweet();
+            tweet.setCreatedAt(status.getCreatedAt());
+            tweet.setText(status.getText());
+            tweet.setLanguage(status.getLang());
+            tweet.setKeywordUsed(statusItemReader.getKeyword());
+            listOfTweets.add(tweet);
+        }
+        return listOfTweets;
+    }
+}
---
+++ src/test/java/com/codingnomads/betty/data/batch/ProcessorTests.java
@@ -0,0 +1,55 @@
+package com.codingnomads.betty.data.batch;
+
+import com.codingnomads.betty.data.batch.models.StatusItemReader;
+import com.codingnomads.betty.data.models.Tweet;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import twitter4j.Status;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProcessorTests {
+
+    @Mock
+    private StatusItemReader mockStatusItemReader;
+    private Processor testProcessor;
+
+    @Before
+    public void setUp() throws Exception {
+        mockStatusItemReader = mock(StatusItemReader.class);
+        testProcessor = new Processor(mockStatusItemReader);
+    }
+
+    @Test
+    public void whenProcessorIsCalledWithListOfStatus_returnsAListOfTweets() throws Exception {
+        List<Status> mockListOfStatus = new ArrayList<>();
+        List<Tweet> expectedTweetsList = new ArrayList<>();
+
+        when(mockStatusItemReader.getKeyword()).thenReturn("testWord");
+        assertThat(testProcessor.process(mockListOfStatus)).isEqualTo(expectedTweetsList);
+    }
+
+    @Test
+    public void whenProcessorIsCalledWithNullListOfStatus_returnsANullListOfTweets() throws Exception {
+        List<Status> mockListOfStatus = new ArrayList<>();
+        mockListOfStatus.add(null);
+
+        List<Tweet> expectedTweetsList = Collections.emptyList();
+
+        when(mockStatusItemReader.getKeyword()).thenReturn("testWord");
+        assertThat(testProcessor.process(mockListOfStatus)).isEqualTo(expectedTweetsList);
+
+    }
+}
