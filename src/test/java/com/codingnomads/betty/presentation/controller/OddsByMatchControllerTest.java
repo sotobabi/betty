bf8925c1fcbb53b93e6ae1d094a4e36692ad1f6b
Cristian Perez <cristian.perez2011@gmail.com>
2019-05-09 14:57:40 +0300

    Changed tests to take mocks within testService constructor and methods

--- src/test/java/com/codingnomads/betty/presentation/controller/OddsByMatchControllerTest.java
+++ src/test/java/com/codingnomads/betty/presentation/controller/OddsByMatchControllerTest.java
@@ -1,21 +1,28 @@
 package com.codingnomads.betty.presentation.controller;
 
+import com.codingnomads.betty.logic.services.GameInformationService;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.ui.Model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class OddsByMatchControllerTest {
 
     private OddsByMatchController testController;
+    private GameInformationService mockGameInformationService;
+    private Model mockModel;
 
     @Before
     public void setUp() {
-        testController = new OddsByMatchController();
+        mockGameInformationService = mock(GameInformationService.class);
+        mockModel = mock(Model.class);
+        testController = new OddsByMatchController(mockGameInformationService);
     }
 
     @Test
     public void whenGetOddsByMatchIsCalled_oddsByMatchIsReturned() {
-        assertThat(testController.getOddsByMatch()).isEqualTo("odds-by-match");
+        assertThat(testController.getOddsByMatch(mockModel)).isEqualTo("odds-by-match");
     }
 }
