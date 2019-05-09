1f357bbe461889bff7ab75787312a1d53fea28a0
Cristian Perez <cristian.perez2011@gmail.com>
2019-05-09 14:52:20 +0300

    Modified native SQL query in order to pull only the most recent results

--- src/main/java/com/codingnomads/betty/logic/interfaces/MatchOddsJpaRepository.java
+++ src/main/java/com/codingnomads/betty/logic/interfaces/MatchOddsJpaRepository.java
@@ -2,9 +2,21 @@ package com.codingnomads.betty.logic.interfaces;
 
 import com.codingnomads.betty.data.models.MatchOdds;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MatchOddsJpaRepository extends JpaRepository<MatchOdds, Long> {
 
     @Override
     MatchOdds save(MatchOdds matchOdds);
+
+    @Override
+    @Query(value = "SELECT t.id, t.away_team, t.away_team_odd, t.home_team, t.home_team_odd, t.match_date FROM match_odds t " +
+            "INNER JOIN (" +
+            "SELECT max(id) AS MaxId, away_team " +
+            "FROM match_odds " +
+            "GROUP BY away_team" +
+            ") tm ON t.id = tm.MaxId AND t.away_team = tm.away_team", nativeQuery = true)
+    List<MatchOdds> findAll();
 }
