610ed6915c120831aac13673a0bdca14e5e300af
Eileen Ai <eileen.aix@gmail.com>
2019-05-12 17:19:07 -0400

    stream api

--- src/main/java/com/codingnomads/betty/configurations/TwitterConfig.java
+++ src/main/java/com/codingnomads/betty/configurations/TwitterConfig.java
@@ -3,22 +3,28 @@ package com.codingnomads.betty.configurations;
 import org.springframework.stereotype.Component;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 @Component
 public class TwitterConfig {
 
     private Twitter twitter;
+    private TwitterStream twitterStream;
     private TwitterFactory twitterFactory;
 
     public TwitterConfig() {
 
         buildTwitterConfiguration();
+        buildTwitterStreamConfiguration();
     }
 
     public Twitter getTwitter() {
         return twitter;
     }
+
+    public TwitterStream getTwitterStream() {return twitterStream;}
 
     private void buildTwitterConfiguration() {
 
@@ -33,6 +39,20 @@ public class TwitterConfig {
 
         twitterFactory = new TwitterFactory(configurationBuilder.build());
         twitter = twitterFactory.getInstance();
+
+    }
+
+    private void buildTwitterStreamConfiguration(){
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+        configurationBuilder
+                .setDebugEnabled(true)
+                .setOAuthConsumerKey(System.getenv("CONSUMER_KEY"))
+                .setOAuthConsumerSecret(System.getenv("CONSUMER_SECRET"))
+                .setOAuthAccessToken(System.getenv("ACCESS_TOKEN"))
+                .setOAuthAccessTokenSecret(System.getenv("ACCESS_TOKEN_SECRET"));
+
+        twitterStream = new TwitterStreamFactory(configurationBuilder.build()).getInstance();
     }
 }
 
--- src/main/java/com/codingnomads/betty/data/api/TwitterSourceMinerRepository.java
+++ src/main/java/com/codingnomads/betty/data/api/TwitterSourceMinerRepository.java
@@ -14,13 +14,12 @@ public class TwitterSourceMinerRepository implements TwitterMinerRepository {
 
     private TwitterConfig twitterConfigurer;
 
-
     @Autowired
     public TwitterSourceMinerRepository(TwitterConfig twitterConfigurer) {
         this.twitterConfigurer = twitterConfigurer;
     }
 
-    public List<Status> searchTweets(String keyword, int numberOfStatus){
+    public List<Status> searchTweets(String keyword, int numberOfStatus) {
 
         Twitter twitter = twitterConfigurer.getTwitter();
         Query query = new Query(keyword);
@@ -37,18 +36,51 @@ public class TwitterSourceMinerRepository implements TwitterMinerRepository {
     }
 
     public void searchTweetsByUserId(String keyword) {
-        Twitter twitter = twitterConfigurer.getTwitter();
-        long userId1 = 265902729;
-        long userId2 = 19230601;
 
-        ResponseList<User> userInfo = null;
-        try {
-            userInfo = twitter.lookupUsers(new long[]{userId1, userId2});
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
-        for(User u: userInfo){
-            System.out.println(u.getScreenName() + ": " + u.getStatus().getText());
-        }
+        TwitterStream twitterStream = twitterConfigurer.getTwitterStream();
+        twitterStream.addListener(setUpSetUpListener());
+
+        FilterQuery filterQuery = new FilterQuery();
+        filterQuery.language("EN");
+        String[] queries = {"Chelsea -Arsenal from:ChelseaFC", "Manchester City from:BBCSport"};
+        filterQuery.track(queries);
+
+        twitterStream.filter(filterQuery);
+        twitterStream.sample();
+    }
+
+    private StatusListener setUpSetUpListener() {
+        return new StatusListener() {
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void onStatus(Status status) {
+                System.out.println("--------Inside OnStatus-----");
+                System.out.println(status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                System.out.println(warning);
+            }
+        };
     }
 }
