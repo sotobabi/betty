927f0471cea5acaa889ce4c45cd77c150fe2ccf6
Eileen Ai <eileen.aix@gmail.com>
2019-05-14 21:10:42 -0400

    queryconstructor class wip

---
+++ src/main/java/com/codingnomads/betty/logic/services/TwitterQueryConstructor.java
@@ -0,0 +1,11 @@
+package com.codingnomads.betty.logic.services;
+
+public class TwitterQueryConstructor {
+
+    //MatchOdds -> finds the corresponding teams' hashtags and accounts and construct query
+
+    //teamName -awayTeamName from a list of accounts
+    //awayTeamName -teamName from a list of accounts
+
+
+}
